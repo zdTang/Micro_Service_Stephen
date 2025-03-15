19dcc49c487ca6f248f4c6a77f967e7ec02e753c
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2025-03-15 11:20:50 -0400

    2-7 Quick Comments Test

--- blogs/comments/index.js
+++ blogs/comments/index.js
@@ -1,26 +1,30 @@
-const express = require('express');
-const { randomBytes } = require('crypto');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const { randomBytes } = require("crypto");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 const commentsByPostId = {};
 
-app.get('/posts/:id/comments', (req, res) => {
+app.get("/posts/:id/comments", (req, res) => {
+  console.log(commentsByPostId);
+  console.log(req.params.id);
   res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post('/posts/:id/comments', (req, res) => {
-  const commentId = randomBytes(4).toString('hex');
+app.post("/posts/:postId/comments", (req, res) => {
+  const commentId = randomBytes(4).toString("hex");
+  console.log(req);
   const { content } = req.body;
   const comments = commentsByPostId[req.params.postId] || [];
   comments.push({ id: commentId, content });
+  console.log(req.params.postId);
   commentsByPostId[req.params.postId] = comments;
   res.status(201).send(comments);
 });
 
 app.listen(4001, () => {
-  console.log('Server is running on port 4001');
+  console.log("Server is running on port 4001");
 });
