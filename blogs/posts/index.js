ec31ccd1d1cbe46a100a4f3a75aa20cf2076f335
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2025-03-15 09:25:06 -0400

    2-4 Posts Service Creation

---
+++ blogs/posts/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const { randomBytes } = require('crypto');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.json());
+
+const posts = [];
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/posts', (req, res) => {
+  res.send(posts);
+});
+
+app.post('/posts', (req, res) => {
+  const id = randomBytes(4).toString('hex');
+  const title = req.body.title;
+  posts[id] = {
+    id,
+    title,
+  };
+  res.status(201).send(posts[id]);
+});
+
+app.listen(4000, () => {
+  console.log('Server is running on port 4000');
+});
