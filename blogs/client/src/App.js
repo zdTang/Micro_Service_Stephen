6a8c9f6959320469dd5eee5d2d1fc2bbd76db575
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2025-03-15 17:44:49 -0400

    2-13 Fetch and Render Post

--- blogs/client/src/App.js
+++ blogs/client/src/App.js
@@ -1,9 +1,12 @@
 import PostCreate from "./PostCreate";
+import PostList from "./PostList";
 const App = () => {
   return (
     <div>
       <>
         <PostCreate />
+        <hr />
+        <PostList />
       </>
     </div>
   );
